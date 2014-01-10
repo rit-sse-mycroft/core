fb60382987dffc1198d6362069e014e210fe0bc6
Derek Erdmann <derek@derekerdmann.com>
2014-01-10 11:25:41 -0500

    Add properties to AppInstance

--- Mycroft/App/AppInstance.cs
+++ Mycroft/App/AppInstance.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Mycroft.App
 {
+    /// <summary>
+    /// Represents an instance of an app that is connected to Mycroft
+    /// </summary>
     class AppInstance
     {
+        /// <summary>
+        /// The name of the app that's running.
+        /// </summary>
+        public String Name { get; private set; }
+
+        /// <summary>
+        /// The pretty name of the app
+        /// </summary>
+        public String DisplayName { get; private set; }
+
+        /// <summary>
+        /// The unique instance ID of the app
+        /// </summary>
+        public String InstanceId { get; private set; }
+
+        /// <summary>
+        /// The Mycroft API version the app expects
+        /// </summary>
+        public uint? ApiVersion { get; private set; }
+
+        /// <summary>
+        /// The current status of the app
+        /// </summary>
+        public Status Status { get; private set; }
+
+        /// <summary>
+        /// The version of the app
+        /// </summary>
+        public Version version { get; set; }
+
     }
 }
