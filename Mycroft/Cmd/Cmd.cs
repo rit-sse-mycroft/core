55990eff440aaf6201561e6361df770ae2be73d8
jlamere <jennielamere@gmail.com>
2014-01-10 13:28:35 -0800

    Parse separates type and turns data into a dictionary

--- Mycroft/Cmd/Cmd.cs
+++ Mycroft/Cmd/Cmd.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,26 @@ namespace Mycroft.Cmd
         /// </returns>
         public static Cmd Parse(String input)
         {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Dictionary<String, Object>)); 
+            int index = input.IndexOf('{');
+            String type;
+            Object data;
+            if (index >= 0)
+            {
+                type = input.Substring(0, index);
+                String rawData = input.Substring(index + 1);
+                var memoryStream = new MemoryStream(Encoding.Unicode.GetBytes(rawData));
+                data = ser.ReadObject(memoryStream);
+            }
+            else
+            {
+                type = input; /// no body was supplied
+            }
+            if(type == "") /// malformed message
+                           /// TODO alert client
+            {
+
+            }
             // Break the message body into the type token and the JSON blob,
             // then delegate to the specific command parser (MsgCmd.Parse(), AppCmd.Parse(), etc.)
             return null;
