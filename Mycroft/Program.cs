77b13be504b22797ef53f2794f330a9be21ec424
John Renner <john@jrenner.net>
2014-01-12 21:40:16 -0500

    Add broken test code to Program.cs

--- Mycroft/Program.cs
+++ Mycroft/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +13,14 @@ namespace Mycroft
     {
         static void Main(string[] args)
         {
-            /// Create and start the main server
+            var myServ = new TlsServer(IPAddress.Any, 1847, new X509Certificate2(X509Certificate2.CreateFromCertFile("mycroft.crt")));
+            myServ.ClientConnected += myServ_ClientConnected;
+            myServ.Start();
+        }
+
+        static void myServ_ClientConnected(object sender, TlsClientStream e)
+        {
+            Debug.Write("Hey World");
         }
     }
 }
