2137cabe5bba08759ad2d7ddbb8e92662b9612da
jlamere <jennielamere@gmail.com>
2014-01-13 13:33:20 -0800

    Added QueryMsgCommand

---
+++ Mycroft.Tests/Cmd/Msg/QueryMsgCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mycroft.Cmd.Msg.Query
+{
+    class QueryMsgCommand : MsgCommand
+    {
+        /// <summary>
+        /// Parses JSON into message command objects
+        /// </summary>
+        /// <param name="messageType">The message type that determines the command to create</param>
+        /// <param name="json">The JSON body of the message</param>
+        /// <returns>Returns a command object for the parsed message</returns>
+ 
+    }
+}
