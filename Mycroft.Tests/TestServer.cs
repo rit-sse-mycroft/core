705f6eb2b7bf6f3da8ad26bba9134f6a2c888c64
Derek Erdmann <derek@derekerdmann.com>
2014-01-14 13:45:53 -0500

    Add certificate thumbprint formatter

--- Mycroft.Tests/TestServer.cs
+++ Mycroft.Tests/TestServer.cs
@@ -7,9 +7,40 @@ namespace Mycroft.Tests
     [TestClass]
     public class TestServer
     {
+        /// <summary>
+        /// Tests that the OpenSSL format is stripped correctly - colons are removed
+        /// and case is preserved
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestFormatCertificateThumbprint_OpenSSLFormat()
         {
+            var original = "17:31:F2:7B:31:35:49:DE:04:CA:19:87:F9:EB:C2:C7:87:42:0B:75";
+            string thumbprint = Server.FormatCertificateThumbprint(original);
+            Assert.AreEqual("1731F27B313549DE04CA1987F9EBC2C787420B75", thumbprint);
+        }
+
+        /// <summary>
+        /// Tests that the Windows certificate viewer format is stripped correctly.
+        /// Removes weird hidden characters and spaces and makes everything uppercase.
+        /// </summary>
+        [TestMethod]
+        public void TestFormatCertificateThumbprint_WindowsFormat()
+        {
+            var original = "‎17 31 f2 7b 31 35 49 de 04 ca 19 87 f9 eb c2 c7 87 42 0b 75";
+            string thumbprint = Server.FormatCertificateThumbprint(original);
+            Assert.AreEqual("1731F27B313549DE04CA1987F9EBC2C787420B75", thumbprint);
+        }
+
+
+        /// <summary>
+        /// Tests that a correctly formatted string is preserved
+        /// </summary>
+        [TestMethod]
+        public void TestFormatCertificateThumbprint_AlreadyFormatted()
+        {
+            var original = "‎1731F27B313549DE04CA1987F9EBC2C787420B75";
+            string thumbprint = Server.FormatCertificateThumbprint(original);
+            Assert.AreEqual("1731F27B313549DE04CA1987F9EBC2C787420B75", thumbprint);
         }
     }
 }
--- Mycroft/Server.cs
+++ Mycroft/Server.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Mycroft
 {
+    /// <summary>
+    /// Starts Mycroft's network communications and owns resources in the server
+    /// </summary>
     class Server
     {
+
+        /// <summary>
+        /// Formats a SHA-1 hash of the certificate (thumbprint) to be used for searching 
+        /// in the user's certificate store
+        /// </summary>
+        /// <returns>
+        /// Returns the thumbprint, stripped of non-alphanumeric characters and capitalized
+        /// </returns>
+        internal static string FormatCertificateThumbprint(string thumbprint)
+        {
+            return Regex.Replace(thumbprint, @"[^a-zA-Z0-9]","", RegexOptions.IgnoreCase).ToUpper();
+        }
     }
 }
