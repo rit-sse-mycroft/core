f7fa0eb439ab207b94066865d4303bfa7e2dbb56
Robert McLaughlin <robert@sparkk.us>
2014-01-17 16:11:14 -0500

    MsgBroadcast uses dynamic for loading content

--- Mycroft.Messages.Test/Msg/MsgBroadcastTest.cs
+++ Mycroft.Messages.Test/Msg/MsgBroadcastTest.cs
@@ -2,30 +2,51 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Runtime.Serialization.Json;
 using System.IO;
+using System.Collections.Generic;
 using System.Text;
+using System.Web.Helpers;
+using Mycroft.Messages.Msg;
 
 namespace Mycroft.Messages.Test.Msg
 {
     [TestClass]
-    public class MsgBroadcastTest 
+    public class MsgBroadcastTest
     {
         private string SampleQuery = @"
-            MSG_BROADCAST {
+            {
                 ""id"" : ""uuid"",
                 ""content"" : {
-                    ""text"": ""pickle unicorn""
+                    ""text"": ""pickle unicorn"",
+                    ""thing"": {
+                        ""other"" : ""blah""
+                    }
                 }
             }";
 
         [TestMethod]
         public void TestMsgBroadcastSerialization()
         {
+            var msgBroadcast = new MsgBroadcast();
+            msgBroadcast.Id = "uuid";
+            var dct = new Dictionary<string, object>();
+            dct.Add("text", "pickle unicorn");
+            var innerDct = new Dictionary<string, object>();
+            innerDct.Add("other", "blah");
+            dct.Add("thing", innerDct);
+            msgBroadcast.Content = dct;
+
+            string json = msgBroadcast.Seralize();
+            System.Diagnostics.Debug.WriteLine(json);
+
+            Assert.IsTrue(json.IndexOf("\"other\":\"blah\"") > 0, "should have inner dict content");
         }
 
         [TestMethod]
         public void TestMsgBroadcastDeserialization()
         {
-        
+            var msgBroadcast = MsgBroadcast.DeSeralize(SampleQuery) as MsgBroadcast;
+            Assert.AreEqual("uuid", msgBroadcast.Id, "should have correct uuid");
+            Assert.AreNotEqual(null, msgBroadcast.Content as object, "should have content");
         }
     }
 }
--- Mycroft.Messages/Msg/MsgBroadcast.cs
+++ Mycroft.Messages/Msg/MsgBroadcast.cs
@@ -4,25 +4,47 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.IO;
 using Mycroft.Messages;
 
 
 namespace Mycroft.Messages.Msg
 {
-    [DataContract]
-    class MsgBroadcast : DataPacket
+    public class MsgBroadcast : DataPacket
     {
 
-        [DataMember(Name = "id", IsRequired = true)]
         public string Id { get; set; }
 
-        [DataMember(Name = "content", IsRequired = true)]
-        public string Content { get; set; }
+        public dynamic Content { get; set; }
 
 
         override
-        public string Seralize(){return null;}
+        public string Seralize()
+        {
+            var dct = new Dictionary<string, object>();
+            dct["id"] = Id;
+            dct["content"] = Content;
+            var obj = new DynamicJsonObject(dct);
+            var writer = new StringWriter();
+            Json.Write(obj, writer);
+            return writer.ToString();
+        }
 
-        public static DataPacket DeSeralize(string json) { return null; }
+        public static DataPacket DeSeralize(string json)
+        {
+            try
+            {
+                var ret = new MsgBroadcast();
+                dynamic obj = Json.Decode(json);
+                ret.Id = obj["id"];
+                ret.Content = obj["content"];
+                return ret;
+            }
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                return null;
+            }
+        }
     }
 }
