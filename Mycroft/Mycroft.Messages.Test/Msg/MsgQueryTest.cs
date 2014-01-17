15e7db1f7d199c7efb02877bf06e2b3adbc4459d
Robert McLaughlin <robert@sparkk.us>
2014-01-17 11:49:12 -0500

    add test and fix 'data' property of MsgQuery

--- Mycroft/Mycroft.Messages.Test/Msg/MsgQueryTest.cs
+++ Mycroft/Mycroft.Messages.Test/Msg/MsgQueryTest.cs
@@ -2,6 +2,9 @@
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using Mycroft.Messages.Msg;
 
 namespace Mycroft.Messages.Test.Msg
 {
@@ -9,15 +12,15 @@ namespace Mycroft.Messages.Test.Msg
     public class MsgQueryTest
     {
         private string SampleQuery = @"
-         MSG_QUERY {
+         {
           ""id"" : ""uuid"",
           ""capability"" : ""weather"",
           ""action"" : ""get_temperature"",
+          ""instanceId"" : [""xxxx"", ""xx2""],
           ""data"" : {
-             ""scale"": ""fahrenheit"",
+             ""scale"" : ""fahrenheit"",
              ""other"" : ""thing""
           },
-          ""instanceId"" : [""xxxx""],
           ""priority"" : 30
         }";
 
@@ -29,6 +32,20 @@ namespace Mycroft.Messages.Test.Msg
         [TestMethod]
         public void TestMsgQueryDeserialization()
         {
+            var settings = new DataContractJsonSerializerSettings();
+            settings.UseSimpleDictionaryFormat = true;
+            var serializer = new DataContractJsonSerializer(typeof(MsgQuery), settings);
+            var memStream = new MemoryStream(Encoding.UTF8.GetBytes(SampleQuery));
+            MsgQuery msgQuery = serializer.ReadObject(memStream) as MsgQuery;
+
+            Assert.AreEqual("uuid", msgQuery.Id);
+            Assert.AreEqual("weather", msgQuery.Capability);
+            Assert.AreEqual("get_temperature", msgQuery.Action);
+            Assert.AreEqual(30, msgQuery.Priority);
+            Assert.AreEqual("xxxx", msgQuery.InstanceId[0]);
+            Assert.AreEqual("xx2", msgQuery.InstanceId[1]);
+            Assert.AreEqual("fahrenheit", msgQuery.Data["scale"]);
+            Assert.AreEqual("thing", msgQuery.Data["other"]);
         }
     }
 }
