3f9021dd375ff130e53d1691ce9fbc90768313c7
Robert McLaughlin <robert@sparkk.us>
2014-01-17 16:49:31 -0500

    add abstract class AppBase

---
+++ Mycroft.Messages/App/AppBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mycroft.Messages;
+
+namespace Mycroft.Messages.App
+{
+    public abstract class AppBase : DataPacket
+    {
+    }
+}
