103cc3f38ec5e3b1f42e4e77ecac9e6123960fcb
Robert McLaughlin <robert@sparkk.us>
2014-01-17 11:21:23 -0500

    add MsgQuery contract

---
+++ Mycroft.Messages/Msg/MsgQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Mycroft.Messages.Msg
+{
+    [DataContract]
+    public class MsgQuery
+    {
+        [DataMember(Name = "id", IsRequired = true)]
+        public string Id { get; set; }
+
+        [DataMember(Name = "capability", IsRequired = true)]
+        public string Capability { get; set; }
+
+        [DataMember(Name = "action", IsRequired = true)]
+        public string Action { get; set; }
+
+        [DataMember(Name = "data", IsRequired = true)]
+        public string Data { get; set; }
+
+        [DataMember(Name = "instanceId", IsRequired = true)]
+        public List<string> InstanceId { get; set; }
+
+        [DataMember(Name = "priority", IsRequired = true)]
+        public int Priority { get; set; }
+    }
+}
---
+++ Mycroft/Mycroft.Messages.Test/Msg/MsgQueryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mycroft.Messages.Test.Msg
+{
+    [TestClass]
+    public class MsgQueryTest
+    {
+        private string SampleQuery = @"
+         MSG_QUERY {
+          ""id"" : ""uuid"",
+          ""capability"" : ""weather"",
+          ""action"" : ""get_temperature"",
+          ""data"" : {
+             ""scale"": ""fahrenheit"",
+             ""other"" : ""thing""
+          },
+          ""instanceId"" : [""xxxx""],
+          ""priority"" : 30
+        }";
+
+        [TestMethod]
+        public void TestMsgQuerySerialization()
+        {
+        }
+
+        [TestMethod]
+        public void TestMsgQueryDeserialization()
+        {
+        }
+    }
+}
