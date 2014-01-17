2a056624a419c6a5b4a5f1af9bb6d89b833f48b1
atd3873 <atd3873@rit.edu>
2014-01-17 11:55:54 -0500

    add MsgBroadcast parser stuff

---
+++ Mycroft.Messages/Msg/MsgBroadcast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mycroft.Messages.Msg
+{
+    [DataContract]
+    class MsgBroadcast
+    {
+
+        [DataMember(Name = "id", IsRequired = true)]
+        public string Id { get; set; }
+
+        [DataMember(Name = "content", IsRequired = true)]
+        public string Content { get; set; }
+
+    }
+}
