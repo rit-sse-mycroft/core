06a2da239fa0f3a7fe4bc84a0e3f9d821b64cd7f
Robert McLaughlin <robert@sparkk.us>
2014-01-20 13:31:57 -0500

    parameter rename

--- Mycroft/Cmd/App/AppCommand.cs
+++ Mycroft/Cmd/App/AppCommand.cs
@@ -15,18 +15,18 @@ namespace Mycroft.Cmd.App
         /// <param name="messageType">The message type that determines the command to create</param>
         /// <param name="json">The JSON body of the message</param>
         /// <returns>Returns a command object for the parsed message</returns>
-        public static Command Parse(String type, String rawData, AppInstance instance)
+        public static Command Parse(String type, String json, AppInstance instance)
         {
             switch (type)
             {
                 case "APP_UP":
-                    return new Up(rawData, instance);
+                    return new Up(json, instance);
                 case "APP_DOWN":
-                    return new Down(rawData, instance);
+                    return new Down(json, instance);
                 case "APP_CREATE":
-                    return new Create(rawData, instance);
+                    return new Create(json, instance);
                 case "APP_DESTROY":
-                    return new Destroy(rawData, instance);
+                    return new Destroy(json, instance);
                 default:
                     //data is incorrect - can't do anything with it
                     // TODO notify that is wrong
