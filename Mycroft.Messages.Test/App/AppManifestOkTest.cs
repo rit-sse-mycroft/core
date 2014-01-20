eeafb632e401d87ab4ddcc8fc3814c28055bc835
Robert McLaughlin <robert@sparkk.us>
2014-01-20 12:03:16 -0500

    add AppManifestOk message

---
+++ Mycroft.Messages.Test/App/AppManifestOkTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mycroft.Messages.App;
+
+namespace Mycroft.Messages.Test.App
+{
+    [TestClass]
+    public class AppManifestOkTest
+    {
+        string SampleManifestOk = @"
+        {
+            ""instanceId"" : ""inst101""
+        }
+        ";
+
+        [TestMethod]
+        public void TestAppManifestOkDeSerialization()
+        {
+            var mfstOk = AppManifestOk.DeSerialize(SampleManifestOk) as AppManifestOk;
+            Assert.AreEqual("inst101", mfstOk.InstanceId);
+        }
+
+        [TestMethod]
+        public void TestAppManifestOkSerialization()
+        {
+            var mfstOk = new AppManifestOk();
+            mfstOk.InstanceId = "inst101";
+            string json = mfstOk.Serialize();
+
+            Assert.IsTrue(json.IndexOf("\"instanceId\":\"inst101\"") > 0, "Should have inst101 for instanceId");
+        }
+    }
+}
---
+++ Mycroft.Messages/App/AppManifestOk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.IO;
+
+namespace Mycroft.Messages.App
+{
+    public class AppManifestOk : AppBase
+    {
+        public string InstanceId { get; set; }
+
+        public override string Serialize()
+        {
+            var dct = new Dictionary<string, object>();
+            dct.Add("instanceId", InstanceId);
+            var obj = new DynamicJsonObject(dct);
+            var writer = new StringWriter();
+            Json.Write(obj, writer);
+            return writer.ToString();
+        }
+
+        public static DataPacket DeSerialize(string json)
+        {
+            try
+            {
+                var ret = new AppManifestOk();
+                dynamic obj = Json.Decode(json);
+                ret.InstanceId = obj["instanceId"];
+                return ret;
+            }
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                return null;
+            }
+        }
+    }
+}
