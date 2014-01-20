9efaa3490667d802350febb95f375bdc3478d3e0
Robert McLaughlin <robert@sparkk.us>
2014-01-20 11:40:03 -0500

    add AppDependency

---
+++ Mycroft.Messages.Test/App/AppDependencyTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mycroft.Messages.App;
+using System.Collections.Generic;
+
+namespace Mycroft.Messages.Test.App
+{
+    [TestClass]
+    public class AppDependencyTest
+    {
+        string SampleAppDependency = @"
+        {
+          ""Video"" : {
+            ""GoogleTV"" : ""in use"",
+            ""Projector"" : ""down""
+          },
+          ""Speaker"" : {
+            ""SpeakerOne"" : ""up"",
+            ""SpeakerTwo"" : ""up""
+          }
+        }";
+
+        [TestMethod]
+        public void TestAppDependencySerialization()
+        {
+            var dep = new AppDependency();
+            dep.Dependencies = new Dictionary<string, Dictionary<string, string>>();
+            var inner = new Dictionary<string, string>();
+            inner["GoogleTV"] = "in use";
+            dep.Dependencies["Video"] = inner;
+
+            string json = dep.Serialize();
+
+            Assert.IsTrue(json.IndexOf("\"GoogleTV\":\"in use\"") > 0, "should have google tv in use");
+        }
+
+        [TestMethod]
+        public void TestAppDependencyDeSerialization()
+        {
+            var dep = AppDependency.DeSerialize(SampleAppDependency) as AppDependency;
+            Assert.AreEqual(2, dep.Dependencies.Count, "should have 2 capabilities");
+            Assert.AreEqual("in use", dep.Dependencies["Video"]["GoogleTV"]);
+            Assert.AreEqual("down", dep.Dependencies["Video"]["Projector"]);
+
+            Assert.AreEqual("up", dep.Dependencies["Speaker"]["SpeakerOne"]);
+            Assert.AreEqual("up", dep.Dependencies["Speaker"]["SpeakerTwo"]);
+        }
+    }
+}
---
+++ Mycroft.Messages/App/AppDependency.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.IO;
+
+namespace Mycroft.Messages.App
+{
+    public class AppDependency : AppBase
+    {
+        public Dictionary<string, Dictionary<string, string>> Dependencies { get; set; }
+
+        public override string Serialize()
+        {
+            dynamic obj = new DynamicJsonObject(new Dictionary<string, object>());
+
+            // we need to create nested objects that contain the dependency information
+            foreach (string capability in Dependencies.Keys)
+            {
+                dynamic inner = new DynamicJsonObject(new Dictionary<string, object>());
+                foreach (string instanceId in Dependencies[capability].Keys)
+                {
+                    inner[instanceId] = Dependencies[capability][instanceId];
+                }
+                obj[capability] = inner;
+            }
+            var writer = new StringWriter();
+            Json.Write(obj, writer);
+            return writer.ToString();
+        }
+
+        public static DataPacket DeSerialize(string json)
+        {
+            var ret = new AppDependency();
+            ret.Dependencies = new Dictionary<string,Dictionary<string,string>>();
+
+            dynamic obj = Json.Decode(json);
+            DynamicJsonObject djobj = obj as DynamicJsonObject;
+
+            // iterate over each of the capabilities
+            foreach (string capability in djobj.GetDynamicMemberNames())
+            {
+                // iterate over each of the instance ids for this capability
+                DynamicJsonObject inner = obj[capability] as DynamicJsonObject;
+                ret.Dependencies[capability] = new Dictionary<string, string>();
+                foreach (string instanceId in inner.GetDynamicMemberNames())
+                {
+                    string status = obj[capability][instanceId];
+                    ret.Dependencies[capability][instanceId] = status;
+                }
+            }
+            return ret;
+        }
+    }
+}
