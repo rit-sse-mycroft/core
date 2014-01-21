6b059b50c9976629b5da16fcdac19104ec202c13
Robert McLaughlin <robert@sparkk.us>
2014-01-21 11:45:02 -0500

    add MsgGeneralFailure

---
+++ Mycroft.Messages.Test/Msg/MsgGeneralFailureTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mycroft.Messages.Msg;
+
+namespace Mycroft.Messages.Test.Msg
+{
+    [TestClass]
+    public class MsgGeneralFailureTest
+    {
+
+        [TestMethod]
+        public void TestMsgGeneralFailureSerialization()
+        {
+            string target = "{\"received\":\"APP_MANFST {}\",\"message\":\"this is a message\"}";
+            var msg = new MsgGeneralFailure();
+            msg.Received = "APP_MANFST {}";
+            msg.Message = "this is a message";
+            string json = msg.Serialize();
+            Assert.AreEqual(target, json);
+        }
+
+        [TestMethod]
+        public void TestMsgGeneralFailureDeserialization()
+        {
+            string sampleMessage = @"
+            {
+              ""received"" : ""APP_MANFST {}"",
+              ""message"" : ""this is a message""
+            }
+            ";
+            var msg = MsgGeneralFailure.Deserialize(sampleMessage) as MsgGeneralFailure;
+            Assert.AreEqual("APP_MANFST {}", msg.Received);
+            Assert.AreEqual("this is a message", msg.Message);
+        }
+    }
+}
---
+++ Mycroft.Messages/Msg/MsgGeneralFailure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.IO;
+
+namespace Mycroft.Messages.Msg
+{
+    public class MsgGeneralFailure : MsgBase
+    {
+        public string Received { get; set; }
+        public string Message { get; set; }
+
+        public override string Serialize()
+        {
+            var dct = new Dictionary<string, object>();
+            dct["received"] = Received;
+            dct["message"] = Message;
+            var obj = new DynamicJsonObject(dct);
+            var writer = new StringWriter();
+            Json.Write(obj, writer);
+            return writer.ToString();
+        }
+
+        public static new DataPacket Deserialize(string json)
+        {
+            try
+            {
+                var ret = new MsgGeneralFailure();
+                var obj = Json.Decode(json);
+                ret.Message = obj["message"];
+                ret.Received = obj["received"];
+                return ret;
+            }
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException)
+            {
+                return null;
+            }
+        }
+    }
+}
