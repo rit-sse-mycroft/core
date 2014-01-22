10dc40172016f087c303330a90a6167cd198c3f2
Robert McLaughlin <robert@sparkk.us>
2014-01-22 15:58:46 -0500

    add archive command

---
+++ Mycroft/Cmd/Msg/ArchiveCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mycroft.Cmd.Msg
+{
+    class ArchiveCommand : Command
+    {
+        private MsgCommand ToArchive;
+
+        public ArchiveCommand(MsgCommand toArchive)
+        {
+            ToArchive = toArchive;
+        }
+
+        public override void VisitMessageArchive(MessageArchive messageArchive)
+        {
+            if (!messageArchive.TryPostMessage(ToArchive))
+            {
+                Debug.WriteLine("Tried to post to archive but it failed");
+            }
+        }
+    }
+}
